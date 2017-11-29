1a79123ef8e333933d0abaaef17cd4385d338822
Luke Matheny <lukem17@gmail.com>
2017-11-29 23:46:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,11 @@
 function forLoop(array) {
   for(var i = 0; i<25; i++) {
     if(i === 1) {
+      console.log(`I am ${i} strange loop.`)
+    }
+    else {
       console.log(`I am ${i} strange loops.`)
+    }
   }
 
 }
